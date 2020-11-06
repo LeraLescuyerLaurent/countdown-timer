791f620eb82109837e16e2a0d82476952c0c329f
lera lescuyer <lllcommunication@gmx.fr>
2020-11-06 01:57:50 +0100

    affichage de la date dans une div et 1er phase de préparation du calcul de ms D/M/A  H:Min:S

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,23 +1,68 @@
 const months = [
-  "January",
-  "February",
-  "March",
-  "April",
-  "May",
-  "June",
-  "July",
-  "August",
-  "September",
-  "October",
-  "November",
-  "December",
+  "Janvier",
+  "Fevrier",
+  "Mars",
+  "Avril",
+  "Mai",
+  "Jun",
+  "Jullet",
+  "Aout",
+  "Septembre",
+  "Octobre",
+  "Novembre",
+  "Decembre",
 ];
 const weekdays = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday",
+  "Lundi",
+  "Mardi",
+  "Mercredi",
+  "Jeudi",
+  "Vendredi",
+  "Samedi",
+  "Dimanche",
 ];
+
+const giveaway = document.querySelector('.giveaway')
+const deadLine = document.querySelector('.deadline')
+const items = document.querySelector('.deadline-format h4')
+
+/** 
+* écrit la date de fin du compteur dans la div qui a la class ".giveaway" possible d add la date dans new Date()
+*/
+// definit le 31 dec 2020 a 23h59min59s
+let futureDate = new Date(2020, 11, 31, 23, 59, 59);
+console.log(futureDate);
+const year = futureDate.getFullYear();
+const hour = futureDate.getHours();
+const minutes = futureDate.getMinutes();
+const seconds = futureDate.getSeconds();
+let month = futureDate.getMonth()
+// console.log(month);
+month = months[month]
+// console.log(month);
+const date = futureDate.getDate()
+// console.log(date);
+const weekday = weekdays[futureDate.getDay()];
+// console.log(weeckday);
+// console.log(weekday);
+giveaway.textContent = `fin de l'année le ${weekday} ${date} ${month} ${year} ${hour}:${minutes}:${seconds}`;
+
+/** future time in ms */
+const futureTime = futureDate.getTime()
+console.log(futureTime);
+  /** 1s = 1000ms  1m = 60s  1h = 60min  1day = 24hr */
+
+function getRemainingTime() {
+  const today = new Date().getTime();
+  console.log(today);
+  const t = futureTime - today
+  console.log(t);
+
+  /**valeur en ms */
+
+  const oneDay = 24*60*60*1000;
+  console.log(oneDay);
+
+}
+
+getRemainingTime() 
